df950cdd6a68b64a4f14816f67ce6d0d559e2e83
Bitts <marcelo.valvassori@gmail.com>
2022-01-05 08:25:40 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,31 @@
 <?php
 
 /**
-* Create By Marcelo Valvassori Bittencourt
-* 01/01/2022 - Exportação de alunos matriculados para o formato CSV
-*/
+ * index.php - Moodle main library
+ * Create By Marcelo Valvassori Bittencourt
+ * 01/01/2022 - Exportação de alunos matriculados para o formato CSV
+ *
+ *
+ * @package    core
+ * @subpackage lib
+ * @copyright  1999 onwards Marcelo Valvassori Bittencourt https://github.com/bitts
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 require(__DIR__.'/../config.php');
 
